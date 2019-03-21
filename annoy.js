4a193498ecf17c1ecdd540681c5cee08027aaace
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2019-03-20 21:25:10 -0700

    Add files via upload

---
+++ annoy.js
@@ -0,0 +1,20 @@
+//var answer = prompt("Are we there yet");
+
+//while(answer == "yes") {
+//  var answer = prompt("are we there yet");
+//}
+
+//  alert("Yay, we made it");
+
+var answer = prompt("Are we there yet?");
+
+while(answer.indexOf("yes") === -1) {
+  var answer = prompt("are we there yet");
+}
+
+alert("yay we made it");
+
+
+///else {
+//  var answer = prompt("are we there yet");
+//} ///
